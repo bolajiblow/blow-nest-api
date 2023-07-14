b48159d34ba908706babccf0a75c14f669a0cdaa
BOLAJI MUILI <bolajimuili@gmail.com>
2023-07-14 10:23:50 +0100

    e2e tests

--- src/auth/decorator/get-user.decorator.ts
+++ src/auth/decorator/get-user.decorator.ts
@@ -1,11 +1,10 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const GetUser = createParamDecorator(
-  (data : string | undefined, ctx: ExecutionContext) => {
+  (data: string | undefined, ctx: ExecutionContext) => {
     const request: Express.Request = ctx.switchToHttp().getRequest();
-    if (data && request.user != undefined ){
-
-        return  request.user[data as keyof typeof request.user] 
+    if (data && request.user != undefined) {
+      return request.user[data as keyof typeof request.user];
     }
     return request.user;
   },
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,64 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-
-describe('AppController (e2e)', () => {
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from '../src/auth/dto/auth.dto';
+import Spec from 'pactum/src/models/Spec';
+describe('App e2e', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
+    await app.listen(3400);
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+  });
+  afterAll(() => {
+    app.close();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('Auth', () => {
+    describe('Signup', () => {
+      it('should signup', () => {
+        const dto: AuthDto = {
+          email: 'blow@gmail.com',
+          password: 'blow',
+        };
+        pactum
+          .spec()
+          .post('http://localhost:3400/auth/signup')
+          .withBody(dto)
+          .expectStatus(201)
+          .inspect();
+
+      });
+    });
+    describe('Signin', () => {
+      it.todo('should signin');
+    });
+  });
+  describe('User', () => {
+    describe('Get me', () => {});
+  });
+  describe('Bookmarks', () => {
+    describe('Create bookmark', () => {});
+
+    describe('Get bookmarks', () => {});
+
+    describe('Get bookmark by id', () => {});
+
+    describe('Edit bookmark by id', () => {});
+
+    describe('Delete bookmark by id', () => {});
   });
 });
